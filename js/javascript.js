de6b84fa68011ac6f5036319fb94e600677aaf0b
Mike <83614654+m-sand@users.noreply.github.com>
2024-03-25 18:34:31 -0400

    Add functions for signle round and computer choice

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,33 @@
 //Gets the computer's choice for the round
 function getComputerChoice() {
-    const arrayPossibleOutcomes = ['Rock', 'Paper', 'Scissors'];
-    const random = Math.floor(math.random * arrayPossibleOutcomes.length);
-    let computerChoice = arrayPossibleOutcomes[random];
-    return computerChoice;
+    const arrayPossibleOutcomes = ["rock", "paper", "scissors"];
+    return arrayPossibleOutcomes[Math.floor(Math.random() * arrayPossibleOutcomes.length)];
+    
 }
+
+//Plays a single round
+function playRound (playerSelection, computerSelection) {
+    let result;
+
+    if (playerSelection.toLowerCase() === computerSelection ) {
+        result = "You Tie - " + playerSelection.toLowerCase() + " and " + computerSelection; 
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper" ) {
+        result = "You Lose - " + computerSelection + " beats " + playerSelection.toLowerCase(); 
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+        result = "You Win - " + playerSelection.toLowerCase() + " beats " + computerSelection; 
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+        result = "You Lose - " + computerSelection + " beats " + playerSelection.toLowerCase(); 
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+        result = "You Win - " + playerSelection.toLowerCase() + " beats " + computerSelection; 
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+        result = "You Lose - " + computerSelection + " beats " + playerSelection.toLowerCase(); 
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+        result = "You Win - " + playerSelection.toLowerCase() + " beats " + computerSelection;  
+    }
+
+    return result;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
